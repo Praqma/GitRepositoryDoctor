1ef90e3a3997320774f33b715a06f93019ee76da
my name <my@email>
2018-10-11 14:02:16 +0200

    Closes #15

--- src/main/java/com/praqma/gitrepodoctor/FindGitRepo.java
+++ src/main/java/com/praqma/gitrepodoctor/FindGitRepo.java
@@ -26,8 +26,8 @@ public class FindGitRepo implements FindGitRepoIF{
         
         try(Stream<Path> fileStream = Files.find(pathToFolder, 999, (p, bfa) -> bfa.isRegularFile())){
             fileStream
-                    .filter(file -> file.getFileName().toString().matches("^((?!.git).)*$"))
-                    .forEach(file -> files.add(file.getFileName().toFile()));
+                    .filter(file -> file.toString().matches("^(?!.*(/.git/|DS_Store)).*$"))
+                    .forEach(file -> files.add(file.toFile()));
             fileStream.close();
         }
         
